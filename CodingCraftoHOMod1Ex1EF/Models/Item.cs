1524a1e3c0c6c7b0075ea50a181a5af258b23190
diegolaquino <diegol.aquino@gmail.com>
2018-11-13 11:33:18 -0200

    Carrinho Implementado

---
+++ CodingCraftoHOMod1Ex1EF/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodingCraftoHOMod1Ex1EF.Models
+{
+    public class Item
+    {
+        public Produto Produto { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
