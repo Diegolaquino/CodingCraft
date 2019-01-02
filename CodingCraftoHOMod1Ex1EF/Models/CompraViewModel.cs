48ae990a4500d6ca2ee8839876cb05ed3fc830b2
diegolaquino <diegol.aquino@gmail.com>
2019-01-02 17:49:53 -0200

    + Microcontabilidade com o balanço

---
+++ CodingCraftoHOMod1Ex1EF/Models/CompraViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingCraftoHOMod1Ex1EF.Models
+{
+    public class CompraViewModel
+    {
+        public string Fornecedor { get; set; }
+
+        public decimal Valor { get; set; }
+    }
+}
