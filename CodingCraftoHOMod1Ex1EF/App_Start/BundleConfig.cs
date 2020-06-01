abc9c2ce2aaadb28a667f2a6a94b4b19c141d749
Diego Aquino <diegol.aquino@gmail.com>
2020-06-01 18:47:17 -0300

    Atualizei os scripts da class bundle e inclui os scripts js e css da página admin de gerenciamento(dashboard)

--- CodingCraftoHOMod1Ex1EF/App_Start/BundleConfig.cs
+++ CodingCraftoHOMod1Ex1EF/App_Start/BundleConfig.cs
@@ -8,6 +8,22 @@ namespace CodingCraftoHOMod1Ex1EF
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables-demo").Include(
+                       "~/Scripts/demo/datatables-demo.js")
+               );
+
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+                        "~/Scripts/demo/chart-area-demo.js")
+                        .Include("~/Scripts/demo/chart-bar-demo.js")
+                        .Include("~/Scripts/demo/chart-pie-demo.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/sb-admin").Include(
+                        "~/Scripts/sb-admin-2.js")
+                        .Include("~/Scripts/sb-admin-2.min.js")
+                );
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-3.3.1.js"));
 
@@ -20,28 +36,37 @@ namespace CodingCraftoHOMod1Ex1EF
             bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
                         "~/Scripts/DataTables/jquery.dataTables.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/datatablesCss").Include(
-                        "~/Content/DataTables/css/jquery.dataTables.min.css"));
+            
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                     "~/Scripts/popper.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css"));
 
-            bundles.Add(new StyleBundle("~/bundles/popper").Include(
-                     "~/Scripts/popper.js"));
 
             bundles.Add(new StyleBundle("~/Content/site").Include(
                      "~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/admin").Include(
+                      "~/Content/admin/sb-admin-2.css")
+                .Include("~/Content/admin/sb-admin-2.min.css")
+                );
+
+            bundles.Add(new StyleBundle("~/bundles/datatablesCss").Include(
+                        "~/Content/DataTables/css/jquery.dataTables.min.css"));
         }
     }
 }
---
+++ CodingCraftoHOMod1Ex1EF/Controllers/AdminsController.cs
@@ -0,0 +1,58 @@
+﻿using CodingCraftoHOMod1Ex1EF.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodingCraftoHOMod1Ex1EF.Controllers
+{
+    //[Authorize]
+    public class AdminsController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public AdminsController()
+        {
+        }
+
+        public AdminsController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Admins
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
